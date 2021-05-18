a782147668d9ab2455486254ac7a6c32f4cc50be
ScriShima <nekitb57@gmail.com>
2021-05-19 01:30:42 +0300

    Selecet a board image

--- app/src/main/java/com/example/caterva/activities/MyProfileActivity.kt
+++ app/src/main/java/com/example/caterva/activities/MyProfileActivity.kt
@@ -26,10 +26,6 @@ import java.net.URI
 
 class MyProfileActivity : BaseActivity() {
 
-    companion object {
-        private const val READ_STORAGE_PERMISSION_CODE = 1
-        private const val PICK_IMAGE_REQUEST_CODE = 2
-    }
 
     private var mSelectedImageFileUri: Uri? = null
     private var mProfileImageURL: String = ""
@@ -48,12 +44,12 @@ class MyProfileActivity : BaseActivity() {
             if (ContextCompat.checkSelfPermission(
                             this, Manifest.permission.READ_EXTERNAL_STORAGE)
                             == PackageManager.PERMISSION_GRANTED) {
-                                showImageChooser()
+                                Constants.showImageChooser(this)
             }else {
                 ActivityCompat.requestPermissions(
                         this,
                         arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-                        READ_STORAGE_PERMISSION_CODE
+                        Constants.READ_STORAGE_PERMISSION_CODE
                 )
             }
         }
@@ -75,10 +71,10 @@ class MyProfileActivity : BaseActivity() {
             grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        if (requestCode == READ_STORAGE_PERMISSION_CODE) {
+        if (requestCode == Constants.READ_STORAGE_PERMISSION_CODE) {
             if (grantResults.isNotEmpty()
                     && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                        showImageChooser()
+                        Constants.showImageChooser(this)
             }
         } else {
             Toast.makeText(
@@ -89,19 +85,10 @@ class MyProfileActivity : BaseActivity() {
         }
     }
 
-    private fun showImageChooser() {
-        var galleryIntent = Intent(
-                Intent.ACTION_PICK,
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-        )
-
-        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST_CODE)
-    }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if(resultCode == Activity.RESULT_OK
-                && requestCode == PICK_IMAGE_REQUEST_CODE
+                && requestCode == Constants.PICK_IMAGE_REQUEST_CODE
                 && data!!.data != null) {
             mSelectedImageFileUri = data.data
 
@@ -177,7 +164,7 @@ class MyProfileActivity : BaseActivity() {
             val sRef: StorageReference =
                 FirebaseStorage.getInstance().reference.child("USER_IMAGE"
                         + System.currentTimeMillis()
-                        + "." + getFileExtension(mSelectedImageFileUri))
+                        + "." + Constants.getFileExtension(this, mSelectedImageFileUri))
 
             sRef.putFile(mSelectedImageFileUri!!).addOnSuccessListener {
                 taskSnapshot ->
@@ -203,11 +190,6 @@ class MyProfileActivity : BaseActivity() {
                 hideProgressDialog()
             }
         }
-    }
-
-    private fun getFileExtension(uri: Uri?): String? {
-        return MimeTypeMap.getSingleton()
-                .getExtensionFromMimeType(contentResolver.getType(uri!!))
     }
 
     fun profileUpdateSuccess() {
---
+++ app/src/main/java/com/example/caterva/models/Board.kt
@@ -0,0 +1,4 @@
+package com.example.caterva.models
+
+class Board {
+}
