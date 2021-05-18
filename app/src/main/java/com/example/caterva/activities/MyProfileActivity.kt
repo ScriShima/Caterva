2a11636c283e88923e72b3d53c50c7c7d83b6de5
ScriShima <nekitb57@gmail.com>
2021-05-18 03:38:18 +0300

    Image Chooser For The Profile Image

--- app/src/main/java/com/example/caterva/activities/MyProfileActivity.kt
+++ app/src/main/java/com/example/caterva/activities/MyProfileActivity.kt
@@ -1,13 +1,33 @@
 package com.example.caterva.activities
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.example.caterva.R
 import com.example.caterva.firebase.FireStoreClass
 import com.example.caterva.models.User
 import kotlinx.android.synthetic.main.activity_my_profile.*
+import java.io.IOException
+import java.lang.Exception
+import java.net.URI
 
 class MyProfileActivity : BaseActivity() {
+
+    companion object {
+        private const val READ_STORAGE_PERMISSION_CODE = 1
+        private const val PICK_IMAGE_REQUEST_CODE = 2
+    }
+
+    private var mSelectedImageFileUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_profile)
@@ -15,12 +35,75 @@ class MyProfileActivity : BaseActivity() {
         setupActionBar()
 
         FireStoreClass().loadUserData(this)
+
+        iv_profile_user_image.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                            this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                            == PackageManager.PERMISSION_GRANTED) {
+                                showImageChooser()
+            }else {
+                ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                        READ_STORAGE_PERMISSION_CODE
+                )
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == READ_STORAGE_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty()
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        showImageChooser()
+            }
+        } else {
+            Toast.makeText(
+                    this,
+                    "Нет доступа"
+                    , Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    private fun showImageChooser() {
+        var galleryIntent = Intent(
+                Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        )
+
+        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK
+                && requestCode == PICK_IMAGE_REQUEST_CODE
+                && data!!.data != null) {
+            mSelectedImageFileUri = data.data
+
+            try {
+                Glide
+                        .with(this)
+                        .load(mSelectedImageFileUri)
+                        .centerCrop()
+                        .placeholder(R.drawable.ic_user_place_holder)
+                        .into(iv_profile_user_image)
+            }catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
     }
 
     private fun setupActionBar() {
         setSupportActionBar(toolbar_my_profile_activity)
         val actionBar = supportActionBar
-        if(actionBar != null) {
+        if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true)
             actionBar.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_ios_24)
             actionBar.title = resources.getString(R.string.my_profile_title)
@@ -35,7 +118,7 @@ class MyProfileActivity : BaseActivity() {
             .load(user.image)
             .centerCrop()
             .placeholder(R.drawable.ic_user_place_holder)
-            .into(iv_user_image)
+            .into(iv_profile_user_image)
 
         et_name.setText(user.name)
         et_email.setText(user.email)
