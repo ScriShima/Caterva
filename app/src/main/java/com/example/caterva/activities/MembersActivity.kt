97638f7a38b7d8305130d81b60644e027f1271ed
ScriShima <nekitb57@gmail.com>
2021-05-21 18:58:55 +0300

    adding new members UI

--- app/src/main/java/com/example/caterva/activities/MembersActivity.kt
+++ app/src/main/java/com/example/caterva/activities/MembersActivity.kt
@@ -1,6 +1,10 @@
 package com.example.caterva.activities
 
+import android.app.Dialog
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.caterva.R
 import com.example.caterva.adapters.MemberListItemsAdapter
@@ -9,6 +13,7 @@ import com.example.caterva.models.Board
 import com.example.caterva.models.User
 import com.example.caterva.utils.Constants
 import kotlinx.android.synthetic.main.activity_members.*
+import kotlinx.android.synthetic.main.dialog_search_member.*
 
 class MembersActivity : BaseActivity() {
 
@@ -51,5 +56,44 @@ class MembersActivity : BaseActivity() {
         }
 
         toolbar_members_activity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_add_member, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_add_member -> {
+                dialogSearchMember()
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun dialogSearchMember() {
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_search_member)
+        dialog.tv_add.setOnClickListener {
+            val name = dialog.et_name_search_member.text.toString()
+
+            if(name.isNotEmpty()) {
+                dialog.dismiss()
+                // TODO implement adding member logic
+            }else {
+                Toast.makeText(
+                    this,
+                    "Пожалуйста введите имя пользователя",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+        dialog.tv_cancel.setOnClickListener {
+            dialog.dismiss()
+        }
+        dialog.show()
     }
 }
